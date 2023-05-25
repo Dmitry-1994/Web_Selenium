447748b53339a35129b663b7ab45930582ff68db
Dmitry T <vizalyyy@gmail.com>
2023-05-25 23:10:24 +0500

    Positive an Negative Tests

--- src/test/java/ru/netology/selenium/CardNegativeTest.java
+++ src/test/java/ru/netology/selenium/CardNegativeTest.java
@@ -12,6 +12,7 @@ public class CardNegativeTest {
     static void setupAll() {
         System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
+
     @BeforeEach
     void startDriver() {
 //        ChromeOptions options = new ChromeOptions();
@@ -20,10 +21,27 @@ public class CardNegativeTest {
 //        options.addArguments("--headless");
         driver = new ChromeDriver();
     }
+
     @AfterEach
     void engDriver() {
         driver.quit();
         driver = null;
+    }
+
+    @Test
+    void emptyFieldAll() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79122518775");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expectedText = "Поле обязательно для заполнения";
+        String expectedColor = "rgba(255, 92, 92, 1)";
+        String actualText = driver.findElement(By.cssSelector("[class].input_invalid span.input__sub")).getText().trim();
+        String actualColor = driver.findElement(By.cssSelector("[class].input_invalid")).getCssValue("color");
+
+        Assertions.assertEquals(expectedText, actualText);
+        Assertions.assertEquals(expectedColor, actualColor);
     }
 
     @Test
@@ -33,10 +51,47 @@ public class CardNegativeTest {
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
 
-        String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='name'] span.input__sub")).getText();
+        String expectedText = "Поле обязательно для заполнения";
+        String expectedColor = "rgba(255, 92, 92, 1)";
+        String actualText = driver.findElement(By.cssSelector("[class].input_invalid span.input__sub")).getText().trim();
+        String actualColor = driver.findElement(By.cssSelector("[class].input_invalid")).getCssValue("color");
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expectedText, actualText);
+        Assertions.assertEquals(expectedColor, actualColor);
+    }
+
+    @Test
+    void invalidNameByLanguage() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Дмитрий Tarasov");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79122518775");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String expectedColor = "rgba(255, 92, 92, 1)";
+        String actualText = driver.findElement(By.cssSelector("[class].input_invalid span.input__sub")).getText().trim();
+        String actualColor = driver.findElement(By.cssSelector("[class].input_invalid")).getCssValue("color");
+
+        Assertions.assertEquals(expectedText, actualText);
+        Assertions.assertEquals(expectedColor, actualColor);
+    }
+
+    @Test
+    void invalidNameBySpecialCharacters() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Дмитрий_Tarasov");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79122518775");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String expectedColor = "rgba(255, 92, 92, 1)";
+        String actualText = driver.findElement(By.cssSelector("[class].input_invalid span.input__sub")).getText().trim();
+        String actualColor = driver.findElement(By.cssSelector("[class].input_invalid")).getCssValue("color");
+
+        Assertions.assertEquals(expectedText, actualText);
+        Assertions.assertEquals(expectedColor, actualColor);
     }
 
     @Test
@@ -46,21 +101,58 @@ public class CardNegativeTest {
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
 
-        String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'] span.input__sub")).getText().trim();
+        String expectedText = "Поле обязательно для заполнения";
+        String expectedColor = "rgba(255, 92, 92, 1)";
+        String actualText = driver.findElement(By.cssSelector("[class].input_invalid span.input__sub")).getText().trim();
+        String actualColor = driver.findElement(By.cssSelector("[class].input_invalid")).getCssValue("color");
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expectedText, actualText);
+        Assertions.assertEquals(expectedColor, actualColor);
     }
+
+    @Test
+    void invalidPhoneByNoPlus() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Дмитрий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("79122518775");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String expectedColor = "rgba(255, 92, 92, 1)";
+        String actualText = driver.findElement(By.cssSelector("[class].input_invalid span.input__sub")).getText().trim();
+        String actualColor = driver.findElement(By.cssSelector("[class].input_invalid")).getCssValue("color");
+
+        Assertions.assertEquals(expectedText, actualText);
+        Assertions.assertEquals(expectedColor, actualColor);
+    }
+
+    @Test
+    void invalidPhoneByCount() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Дмитрий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7912251877");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String expectedColor = "rgba(255, 92, 92, 1)";
+        String actualText = driver.findElement(By.cssSelector("[class].input_invalid span.input__sub")).getText().trim();
+        String actualColor = driver.findElement(By.cssSelector("[class].input_invalid")).getCssValue("color");
+
+        Assertions.assertEquals(expectedText, actualText);
+        Assertions.assertEquals(expectedColor, actualColor);
+    }
+
     @Test
     void emptyFieldAgree() {
         driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Дмитрий");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79122518775");
-        //driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
 
         String expected = "rgba(255, 92, 92, 1)";
-        String actual = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid")).getCssValue("color");
+        String actual = driver.findElement(By.cssSelector("[class].input_invalid")).getCssValue("color");
 
         Assertions.assertEquals(expected, actual);
     }
--- src/test/java/ru/netology/selenium/CardPositiveTest.java
+++ src/test/java/ru/netology/selenium/CardPositiveTest.java
@@ -13,6 +13,7 @@ public class CardPositiveTest {
     static void setupAll() {
         System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
     }
+
     @BeforeEach
     void startDriver() {
 //        ChromeOptions options = new ChromeOptions();
@@ -21,6 +22,7 @@ public class CardPositiveTest {
 //        options.addArguments("--headless");
         driver = new ChromeDriver();
     }
+
     @AfterEach
     void engDriver() {
         driver.quit();
@@ -30,7 +32,7 @@ public class CardPositiveTest {
     @Test
     void nameTest_1() {
         driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Дмитрий");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Дмитрий Тарасов");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79122518775");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
@@ -40,6 +42,19 @@ public class CardPositiveTest {
 
         Assertions.assertEquals(expected, actual);
     }
-}
 
-//[data-test-id='name'] span.input__sub //для поиска сообщения об ошибке что поле обязательно для заполнения
+    @Test
+    void nameTest_2() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Петров Али-Баба");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79122518775");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
